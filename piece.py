48b5a829e4de034c993d1bd55a67df1dbbe93a50
Darren Kearney <github@darrenk.net>
2017-04-02 18:59:22 +0100

    Moved recursive tile scanner to piece.py to be part of the generic piece class. Fixed bug with y axis conditional. 	modified:   piece.py 	modified:   rook.py

--- piece.py
+++ piece.py
@@ -64,6 +64,74 @@ class Piece(object):
         pass
 
 
+    def recursive_tile_scanner( self, board, direction ):
+        # Unfinished
+        # Scans tiles in direction
+
+        x = direction[0]
+        y = direction[1]
+        step_x = direction[2]
+        step_y = direction[3]
+
+
+        # if no direction then just exit
+        if x == 0 and y == 0:
+            return
+
+        # Don't go off the board
+        if self.pos[0] + x >= 8:
+            return
+
+        if self.pos[0] + x <= -1:
+            return
+
+        if self.pos[1] + y >= 8:
+            return
+
+        if self.pos[1] + y <= -1:
+            return
+
+        # Look for pieces in this direction on x axis
+        if x != 0:
+            if self.pos[0] + x <= 7 and self.pos[0] + x >= 0:
+
+                if self.is_opponent_at_tile( board[ self.pos[1] ][ self.pos[0]+x ] ) == True:
+                    # If tile contains opponent, add it to available tiles, break
+                    self.available_tiles.append( (self.pos[0]+x, self.pos[1]) )
+                    return
+
+                elif self.is_piece_at_tile( board[ self.pos[1]][ self.pos[0]+x ] ) == False:
+                    # If tile doesn't contain anything, add it to available tiles
+                    self.available_tiles.append( (self.pos[0]+x, self.pos[1]) )
+
+                elif self.is_piece_at_tile( board[ self.pos[1]][ self.pos[0]+x ] ) == True:
+                    # Encountered a piece that isn't an opponent piece, so break
+                    return
+
+        if y != 0:
+            if self.pos[1] + y <= 7 and self.pos[1] + y >= 0:
+
+                if self.is_opponent_at_tile( board[ self.pos[1]+y ][ self.pos[0] ] ) == True:
+                    # If tile contains opponent, add it to available tiles, break
+                    self.available_tiles.append( (self.pos[0], self.pos[1]+y) )
+                    return
+
+                elif self.is_piece_at_tile( board[ self.pos[1]+y][ self.pos[0] ] ) == False:
+                    # If tile doesn't contain anything, add it to available tiles
+                    self.available_tiles.append( (self.pos[0], self.pos[1]+y) )
+
+                elif self.is_piece_at_tile( board[ self.pos[1]+y][ self.pos[0] ] ) == True:
+                    # Encountered a piece that isn't an opponent piece, so break
+                    return
+
+
+        # Increment/decrement direction by step
+        direction[0] = x + step_x
+        direction[1] = y + step_y
+
+        self.recursive_tile_scanner( board, direction )
+
+
     def set_state_of_piece( self, state_list ):
         self.state_list = state_list
 
