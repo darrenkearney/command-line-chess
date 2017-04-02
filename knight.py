fd2ca6dcc5138bcb56ae046b7fad26291b0bdae1
Darren Kearney <github@darrenk.net>
2017-04-02 20:27:33 +0100

    Added knight's moveset. Loving the recursive tile scanner! 	modified:   knight.py

--- knight.py
+++ knight.py
@@ -12,3 +12,38 @@ class Knight(Piece):
 
         for key, value in kwargs.items():
             setattr( self, key, value )
+
+
+    def get_possible_moves( self, board, player ):
+
+        self.available_tiles = [] # array of (x,y) coordinate tuples
+
+        # A knight moves weirdly. on tile out, then diagonally.
+
+        move_limit = 1
+
+        # Scan up up right
+        self.recursive_tile_scanner( board, [1,2,0,0, move_limit ] )
+
+        # Scan up right right
+        self.recursive_tile_scanner( board, [2,1,0,0, move_limit ] )
+
+        # Scan down right right
+        self.recursive_tile_scanner( board, [2,-1,0,0, move_limit ] )
+
+        # Scan down down right
+        self.recursive_tile_scanner( board, [1,-2,0,0, move_limit ] )
+
+        # Scan down down left
+        self.recursive_tile_scanner( board, [-1,-2,0,0, move_limit ] )
+
+        # Scan down left left
+        self.recursive_tile_scanner( board, [-2,-1,0,0, move_limit ] )
+
+        # Scan up left left
+        self.recursive_tile_scanner( board, [-2,1,0,0, move_limit ] )
+
+        # Scan up up left
+        self.recursive_tile_scanner( board, [-1,2,0,0, move_limit ] )
+
+        return self.available_tiles
